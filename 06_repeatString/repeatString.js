247bc3ec44535094c83eba64344765835a529ece
jp garnier <jpgpgit@gmail.com>
2025-08-15 15:23:34 -0700

    Completed exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    let string = ""; //empty string to start
+    for (let i=0; i<times; i++) {
+        string += str;
+    }
 
+    return string;
 };
+
+console.log(repeatString("hi", 3));
+
 
 // Do not edit below this line
 module.exports = repeatString;
