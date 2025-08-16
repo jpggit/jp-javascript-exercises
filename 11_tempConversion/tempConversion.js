be0e88b3dae820b27c9b00a5a70c05839f4f1503
jp garnier <jpgpgit@gmail.com>
2025-08-15 19:55:59 -0700

    COmpleted task

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let c = (farenheit-32)*5/9
+  let cRounded = Math.round(c * 10) / 10
+  return cRounded;
+}
+
+const convertToFahrenheit = function(celcius) {
+  let f = (celcius*9/5)+32
+  let fRounded = Math.round(f * 10 ) / 10
+  return fRounded;
 };
 
-const convertToFahrenheit = function() {
-};
 
 // Do not edit below this line
 module.exports = {
