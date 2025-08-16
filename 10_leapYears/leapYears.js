3bc8d30c55f265597d863910ed2aaf851a3fb530
jp garnier <jpgpgit@gmail.com>
2025-08-15 19:40:57 -0700

    Completed leap year logic

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ( year % 4 !== 0) return false;
+    if (year % 100 === 0 && year % 400 !== 0) return false;
+    // if (year % 100 === 0 && year % 400 == 0) return true; (NOT NEEDED, REDUNDANT)
+    // if ( year % 4 == 0) return true; (NOT NEEDED, REDUNDANT)
+    return true;
 };
+
+
+// if !num%4 === 0 || num/100===0 = "ERROR";
+// if num / 400 === 0
+
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
