7c813a9ef558e9c368dd8e3c8858d2ca3a36c701
jp garnier <jpgpgit@gmail.com>
2025-08-15 19:21:03 -0700

    Completed 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR"; //check if numbers are integers
+    if (min < 0 || max < 0) return "ERROR";//check if numbers are positive
+    if (min > max){         //switcheroo of values of min > max
+        const hold = min;
+        min = max;
+        max = hold;
+    }
 
-};
+    let addition = 0;
+ 
+    for (let i = min; i<=max; i++ ) {
+        addition += i;
+    }
+
+     return addition;
+}
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
