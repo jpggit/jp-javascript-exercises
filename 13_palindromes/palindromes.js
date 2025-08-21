9ebf5773ef794a051b2772a2b1952480eaf44acf
jp garnier <jpgpgit@gmail.com>
2025-08-21 12:32:23 -0700

    add comments

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,10 +1,10 @@
 const palindromes = function (string) {
 let newString = string.replace(/\s/g, ''); //removes spaces
-let strArray = newString.split('');
-let rev = strArray.reverse();
-let str2 = rev.join('');
+let strArray = newString.split(''); // splits into array of letters
+let rev = strArray.reverse(); // reverses the array
+let str2 = rev.join(''); //joins reversed array
 
-return newString === str2;
+return newString === str2; //checks if space-less array = reversed array
 };
 
 // Do not edit below this line
