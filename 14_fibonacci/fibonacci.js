43ae1afc8fad6f1ed276be5cec8b847abbfeaa5e
jp garnier <jpgpgit@gmail.com>
2025-08-21 13:47:41 -0700

    Completed the fibonacci test using if statements and a for loop that geneates a fibonacci sequence and returns the position of the num.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num, 10);
+    
+    let a = 0
+    let b = 1
+    let temp = 0
 
+    if (num < 0) return "OOPS";
+    if (num === 0 ) return a;
+    if (num ===1 ) return b;
+
+    for (let i = 2; i<=num; i++ ) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
