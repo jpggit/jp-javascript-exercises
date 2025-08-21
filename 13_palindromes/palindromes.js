8908a02d00291bfa47fe140eccaeed60fa7cd5d2
jp garnier <jpgpgit@gmail.com>
2025-08-21 12:30:49 -0700

    Completed palindome test 'let' functions'

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+let newString = string.replace(/\s/g, ''); //removes spaces
+let strArray = newString.split('');
+let rev = strArray.reverse();
+let str2 = rev.join('');
 
+return newString === str2;
 };
 
 // Do not edit below this line
