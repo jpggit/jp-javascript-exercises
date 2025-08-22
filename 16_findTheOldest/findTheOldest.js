ecc7c7f089a848c765b3cf4c79ee4d60207aeaa0
jp garnier <jpgpgit@gmail.com>
2025-08-21 18:47:27 -0700

    Completed the test using logic to calculate age of each person, then finding the oldest using a reduce with a ternary operation

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const findAge = (person) => {
+        if (!person.yearOfDeath) { 
+            return new Date().getFullYear() - person.yearOfBirth;
+        } else {
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+    }
+    const oldestPerson = people.reduce ((oldestSoFar, current) => {
+        return findAge(current)  > findAge(oldestSoFar) 
+        ? current 
+        : oldestSoFar
+    }, people[0]);
+    return oldestPerson.name;
 };
 
 // Do not edit below this line
